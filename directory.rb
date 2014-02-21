5e5f4a70cfe872d365f1cb953e643b407b71be02
Anna <anna.yanova@gmail.com>
2014-02-21 16:25:12 +0000

    Refactored the code to make it more readable

--- directory.rb
+++ directory.rb
@@ -1,35 +1,51 @@
 student_count = 23
 # let's put all students into an array
 students = [
- "Mario Gintili"
- "Mikhail Dubov"
- "Karolis Noreika"
- "Michael Sidon"
- "Charles De Barros"
- "Ruslan Vikhor"
- "Toby Retallick"
- "Mark Mekhaiel"
- "Sarah Young"
- "Hannah Wight"
- "Khushkaran Singh"
- "Rick Brunstedt"
- "Manjit Singh"
- "Alex Gaudiosi"
- "Ross Hepburn"
- "Natascia Marchese"
- "Tiffanie Chia"
- "Matthew Thomas"
- "Freddy McGroarty"
- "Tyler Rollins"
- "Richard Curteis"
- "Anna Yanova"
- "Andrew Cumine"
+{:name => "Mario Gintili", :cohort => :february},
+{:name => "Mikhail Dubov", :cohort => :february},
+{:name => "Karolis Noreika", :cohort => :february},
+{:name => "Michael Sidon", :cohort => :february},
+{:name => "Charles De Barros", :cohort => :february},
+{:name => "Ruslan Vikhor", :cohort => :february},
+{:name => "Toby Retallick", :cohort => :february},
+{:name => "Mark Mekhaiel", :cohort => :february},
+{:name => "Sarah Young", :cohort => :february},
+{:name => "Hannah Wight", :cohort => :february},
+{:name => "Khushkaran Singh", :cohort => :february},
+{:name => "Rick Brunstedt", :cohort => :february},
+{:name => "Manjit Singh", :cohort => :february},
+{:name => "Alex Gaudiosi", :cohort => :february},
+{:name => "Ross Hepburn", :cohort => :february},
+{:name => "Natascia Marchese", :cohort => :february},
+{:name => "Tiffanie Chia", :cohort => :february},
+{:name => "Matthew Thomas", :cohort => :february},
+{:name => "Freddy McGroarty", :cohort => :february},
+{:name => "Tyler Rollins", :cohort => :february},
+{:name => "Richard Curteis", :cohort => :february},
+{:name => "Anna Yanova", :cohort => :february},
+{:name => "Andrew Cumine", :cohort => :february},
 ]
-# and then print them
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.length} great students"
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.length} great students!"
+end
+
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+
+
+
+
